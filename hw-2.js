9357e585d82bc39faf5098bf1a71f941fe7225c3
Ayşegül Kavaklı <aysegulkavakli@Imac-iMac-2.local>
2022-02-11 17:44:11 +0300

    homework2 updated with 'some()' method

--- hw-2.js
+++ hw-2.js
@@ -2,6 +2,6 @@ const accessible = ["admin", "moderator", "superadmin", "guest"];
 
 Array.prototype.includesCi = function (val) {
     //Here we also can use toLowerCase() method
-    return this.map((a) => a.toUpperCase()).includes(val.toUpperCase())
+    return this.some((a) => a.toUpperCase() === val.toUpperCase())
 }
-console.log(accessible.includesCi("adMin"));
+console.log(accessible.includesCi("cefe"));
